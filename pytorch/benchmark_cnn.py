e3b07023df0751ec3f401d4c0e68e4d56b0abbd0
Li, Long <lilong12@baidu.com>
2019-03-07 17:32:48 +0800

    add metrics

--- pytorch/benchmark_cnn.py
+++ pytorch/benchmark_cnn.py
@@ -19,19 +19,21 @@ from __future__ import print_function
 
 import time
 import os
+import shutil
 import numpy as np
 
 import pytorch.models.cnn.alexnet_model as alexnet
-import pytorch.models.cnn.resnet_model as resnet
+#import pytorch.models.cnn.resnet_model as resnet
 #import pytorch.models.cnn.googlenet_model as googlenet
-import pytorch.models.cnn.vgg_model as vgg
+#import pytorch.models.cnn.vgg_model as vgg
 
 import torch
 import torch.multiprocessing as mp
 import torchvision
 
-def get_optimizer(model, params, learning_rate):
+def get_optimizer(model, params):
   """Returns the optimizer that should be used based on params."""
+  learning_rate = params.init_learning_rate
   if params.optimizer == 'momentum':
     opt = torch.optim.SGD(model.parameters(), lr=learning_rate,
                           momentum=params.momentum, nesterov=True)
@@ -49,8 +51,8 @@ def get_optimizer(model, params, learning_rate):
 
 MODEL_CREATOR = {
         'alexnet': alexnet.AlexnetModel,
-        'resnet50': resnet.create_resnet50,
-        'vgg16': vgg.create_vgg16,
+        #'resnet50': resnet.create_resnet50,
+        #'vgg16': vgg.create_vgg16,
     }
 
 class BenchmarkCNN(object):
@@ -66,32 +68,19 @@ class BenchmarkCNN(object):
     """
     self.params = params
 
-    self.model = self.params.model
-    self.batch_size_per_device = self.params.batch_size
-    self.batch_size = self.params.batch_size
+    self.params.total_batch_size = self.params.batch_size
 
-    self.num_gpus = self.params.num_gpus
-    if self.num_gpus:
-      self.batch_size = self.batch_size_per_device * self.params.num_gpus
-
-    self.do_train = self.params.do_train
-    self.do_eval = self.params.do_eval
+    if self.params.num_gpus:
+      self.params.total_batch_size *= self.params.num_gpus
 
     self.num_epochs = self.params.num_epochs
 
-    self.use_synthetic_data = False if self.params.data_dir else True
-    self.data_dir = self.params.data_dir
+    self.params.use_synthetic_data = False if self.params.data_dir else True
 
-    self.use_fp16 = self.params.use_fp16
-
-    if self.use_fp16:
-      self.data_type = torch.float16
+    if self.params.use_fp16:
+      self.params.data_type = torch.float16
     else:
-      self.data_type = torch.float32
-
-    self.data_format = self.params.data_format
-    self.optimizer = self.params.optimizer
-    self.all_reduce_spec = self.params.all_reduce_spec
+      self.params.data_type = torch.float32
 
     self.params.distributed = True if self.params.ip_list else False
     self.params.world_size = 1
@@ -104,151 +93,146 @@ class BenchmarkCNN(object):
       self.params.dist_url = "tcp://" + address
       self.params.dist_backend = "nccl"
     self.params.multiprocessing_distributed = False
-    if self.num_gpus > 1:
+    if self.params.num_gpus > 1:
       self.params.multiprocessing_distributed = True
-    self.params.workers = 4 # Number of data loading workers.
+      self.params.world_size *= self.params.num_gpus
+    self.params.workers = 0 # Number of data loading workers.
 
     self.print_info()
     
   def print_info(self):
     """Print basic information."""
-    dataset_name = "ImageNet-synthetic" if self.use_synthetic_data else (
+    dataset_name = "ImageNet-synthetic" if self.params.use_synthetic_data else (
                    "ImageNet")
     mode = ""
-    if self.do_train:
+    if self.params.do_train:
       mode += 'train '
-    if self.do_eval:
+    if self.params.do_eval:
       mode += 'eval '
 
     print()
-    print('Model:       %s' % self.model)
+    print('Model:       %s' % self.params.model)
     print('Dataset:     %s' % dataset_name)
     print('Mode:        %s' % mode)
     print('Batch size:  %s global (per machine)' % (
-           self.batch_size))
-    print('             %s per device' % (self.batch_size_per_device))
-    print('Num GPUs:    %d per worker' % (self.num_gpus))
-    print('Num epochs:  %d' % self.num_epochs)
-    print('Data format: %s' % self.data_format)
-    print('Optimizer:   %s' % self.optimizer)
-    print('AllReduce:   %s' % self.all_reduce_spec)
+           self.params.total_batch_size))
+    print('             %s per device' % (self.params.batch_size))
+    print('Num GPUs:    %d per worker' % (self.params.num_gpus))
+    print('Num epochs:  %d' % self.params.num_epochs)
+    print('Data format: %s' % self.params.data_format)
+    print('Optimizer:   %s' % self.params.optimizer)
     print('=' * 30)
 
   def run(self):
     """Run the benchmark task assigned to this process."""
     if self.params.multiprocessing_distributed:
-      self.params.world_size = self.num_gpus * self.params.world_size
-      mp.spawn(main_worker, nprocs=self.num_gpus, 
-               args=(self.model, self.num_gpus, 
-                   self.params.distributed, self.params))
+      mp.spawn(main_worker, nprocs=self.params.num_gpus, 
+               args=(self.params))
     else:
-      main_worker(self.model, self.num_gpus, self.params.distributed,
-              self.params)
+      main_worker(0, self.params)
 
-def main_worker(model, num_gpus, distributed, params):
-    model = MODEL_CREATOR[model]()
-    if num_gpus == 1 and not distributed:
-      model = model.cuda(0)
-    else: #self.distributed:
-      model.cuda()
-      model = torch.nn.parallel.DistributedDataParallel(model)
-      self.params.rank = self.params.rank * num_gpus + gpu
-      dist.init_process_group(backend=params.dist_backend,
-              init_method=params.dist_url,
-              world_size=params.world_size,
-              rank=params.rank)
-#    else:
-#      if self.params.model.startswith('alexnet') or self.params.model.startswith('vgg'):
-#        model.features = torch.nn.DataParallel(model.features)
-#        model.cuda()
-#      else:
-#        model = torch.nn.DataParallel(model).cuda()
-#    
-    criterion = torch.nn.CrossEntropyLoss().cuda(0)
-    optimizer = get_optimizer(model, params, params.init_learning_rate)
-#
-    if params.data_dir:
-      checkpoint_file = os.path.join(params.data_dir, 'checkpoint.ckt')
-      if os.path.isfile(checkpoint_file):
-        print("Loading checkpoint: {}".format(checkpoint))
-        checkpoint = torch.load(checkpoint)
-        start_epoch = checkpoint['epoch']
-        top_1_acc = checkpoint['top_1_acc1']
-        model.load_state_dict(checkpoint['state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer'])
-        print("Loaded checkpoint {} at epoch {}"
-                .format(checkpoint, start_epoch))
+def main_worker(gpu_id, params=None):
+  model = MODEL_CREATOR[params.model]()
+  if params.num_gpus == 1 and not params.distributed:
+    model = model.cuda(gpu_id)
+  else: #self.distributed:
+    model.cuda()
+    model = torch.nn.parallel.DistributedDataParallel(model)
+    self.params.rank = self.params.rank * params.num_gpus + gpu_id
+    dist.init_process_group(backend=params.dist_backend,
+            init_method=params.dist_url,
+            world_size=params.world_size,
+            rank=params.rank)
 
-    train_data_dir = os.path.join(params.data_dir, 'train')
-    if params.do_eval:
-      eval_data_dir = os.path.join(params.data_dir, 'eval')
+  criterion = torch.nn.CrossEntropyLoss().cuda(gpu_id)
+  optimizer = get_optimizer(model, params)
 
-    normalized = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                  std=[0.229, 0.224, 0.225])
+  start_epoch = 0
+  if params.data_dir:
+    checkpoint_file = os.path.join(params.data_dir, 'checkpoint.ckt')
+    if os.path.isfile(checkpoint_file):
+      print("Loading checkpoint: {}".format(checkpoint))
+      checkpoint = torch.load(checkpoint)
+      start_epoch = checkpoint['epoch']
+      top_1_acc = checkpoint['top_1_acc1']
+      model.load_state_dict(checkpoint['state_dict'])
+      optimizer.load_state_dict(checkpoint['optimizer'])
+      print("Loaded checkpoint {} at epoch {}"
+              .format(checkpoint, start_epoch))
 
-    train_data = torchvision.datasets.ImageFolder(
-            train_data_dir,
-            torchvision.transforms.Compose([
-                torchvision.transforms.RandomResizedCrop(224),
-                torchvision.transforms.RandomHorizontalFlip(),
-                torchvision.transforms.ToTensor(),
-                normalized]))
+  train_data_dir = os.path.join(params.data_dir, 'train_by_label')
+  if params.do_eval:
+    eval_data_dir = os.path.join(params.data_dir, 'eval_by_label')
 
-    if distributed:
-      train_sampler = torch.utils.data.distributed.DistributedSampler(
-              train_data)
-    else:
-      train_sampler = None
+  normalized = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                std=[0.229, 0.224, 0.225])
 
-    train_data_loader = torch.utils.data.DataLoader(
-            train_data, batch_size=params.batch_size, 
-            shuffle=(train_sampler is None),
-            num_workers=params.workers, pin_memory=True, 
-            sampler=train_sampler)
+  train_data = torchvision.datasets.ImageFolder(
+          train_data_dir,
+          torchvision.transforms.Compose([
+              torchvision.transforms.RandomResizedCrop(224),
+              torchvision.transforms.RandomHorizontalFlip(),
+              torchvision.transforms.ToTensor(),
+              normalized]))
 
-    if params.do_eval:
-      eval_data = torchvision.datasets.ImageFolder(
-            eval_data_dir,
-            torchvision.transforms.Compose([
-                torchvision.transforms.Resize(256),
-                torchvision.transforms.CenterCrop(224),
-                torchvision.transforms.ToTensor(),
-                normalized]))
-            
-      eval_data_loader = torch.utils.data.DataLoader(
-             eval_data, batch_size=params.batch_size, 
-             shuffle=False,
-             num_workers=params.workers, pin_memory=True)
+  if params.distributed:
+    train_sampler = torch.utils.data.distributed.DistributedSampler(
+            train_data)
+  else:
+    train_sampler = None
 
-    start_epoch = 0
-    for epoch in range(start_epoch, int(params.num_epochs)):
-      epoch_start_time = time.time()
-      if params.distributed:
-        train_sampler.set_epoch(epoch)
-      adjust_learning_rate(optimizer, epoch, params)
+  train_data_loader = torch.utils.data.DataLoader(
+          train_data, batch_size=params.batch_size, 
+          shuffle=(train_sampler is None),
+          num_workers=params.workers, pin_memory=True, 
+          sampler=train_sampler)
 
-      # train for one epoch.
-      train(train_data_loader, model, criterion, optimizer, epoch, params)
+  if params.do_eval:
+    eval_data = torchvision.datasets.ImageFolder(
+          eval_data_dir,
+          torchvision.transforms.Compose([
+              torchvision.transforms.Resize(256),
+              torchvision.transforms.CenterCrop(224),
+              torchvision.transforms.ToTensor(),
+              normalized]))
+          
+    eval_data_loader = torch.utils.data.DataLoader(
+           eval_data, batch_size=params.batch_size, 
+           shuffle=False,
+           num_workers=params.workers, pin_memory=True)
 
-      print("Epoch {} ran time: {}".format(
-          epoch, time.time() - epoch_start_time))
-      acc1 = validate(val_loader, model, criterion)
 
-      is_best = acc1 > top_1_acc
-      top_1_acc = max(acc1, top_1_acc)
-      save_checkpoint({
-          'epoch': epoch + 1,
-          'model': params.model,
-          'state_dict': model.state_dict(),
-          'best_acc1': top_1_acc1,
-          'optimizer': optimizer.state_dict(),
-          }, is_best)
+  for epoch in range(start_epoch, int(params.num_epochs)):
+    epoch_start_time = time.time()
+    if params.distributed:
+      train_sampler.set_epoch(epoch)
+    adjust_learning_rate(optimizer, epoch, params)
 
+    # train for one epoch.
+    train(train_data_loader, model, criterion, optimizer, epoch, params)
+
+    print("Epoch {} ran time: {}".format(
+        epoch, time.time() - epoch_start_time))
+    acc1 = validate(val_loader, model, criterion)
+
+    is_best = acc1 > top_1_acc
+    top_1_acc = max(acc1, top_1_acc)
+    save_checkpoint({
+        'epoch': epoch + 1,
+        'model': params.model,
+        'state_dict': model.state_dict(),
+        'best_acc1': top_1_acc1,
+        'optimizer': optimizer.state_dict(),
+        }, is_best)
+
+  if do_eval:
+    validate(eval_data_loader, model, criterion, params)
 
 def train(train_loader, model, criterion, optimizer, epoch, params):
   """Train for one epoch."""
   batch_time = AverageMeter()
   data_time = AverageMeter()
+  train_time = AverageMeter()
   losses = AverageMeter()
   top1 = AverageMeter()
   top5 = AverageMeter()
@@ -274,18 +258,30 @@ def train(train_loader, model, criterion, optimizer, epoch, params):
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-
+    
     batch_time.update(time.time() - end)
+    train_time.update(batch_time.val - data_time.val) 
     end = time.time()
 
-    print("Epoch: [{0}][{1}/{2}]\t"
-          "Time: {batch_time.val: .3f} ({batch_time.avg: .3f})\t"
-          "Data: {data_time.val: .3f} ({data_time.avg: .3f})\t"
-          "Loss: {loss.val: .4f} ({loss.avg: .4f})\t"
-          "Acc@1: {top1.val: .3f} ({top1.avg: .3f})\t"
-          "Acc@5: {top5.val: .3f} ({top5.avg: .3f})\t".format(
-              epoch, i, len(train_loader), batch_time=batch_time,
-              data_time=data_time, loss=losses, top1=top1, top5=top5))
+    #print("=" * 30)
+    #print("Epoch: [{0}][{1}/{2}]\n"
+    #      "Time: {batch_time.val: .3f} ({batch_time.avg: .3f})\n"
+    #      "Data: {data_time.val: .3f} ({data_time.avg: .3f})\n"
+    #      "Loss: {loss.val: .4f} ({loss.avg: .4f})\n"
+    #      "Acc@1: {top1.val: .3f} ({top1.avg: .3f})\n"
+    #      "Acc@5: {top5.val: .3f} ({top5.avg: .3f})\n".format(
+    #          epoch, i, len(train_loader), batch_time=batch_time,
+    #          data_time=data_time, loss=losses, top1=top1, top5=top5))
+    average_examples_per_sec_without_data = params.total_batch_size / train_time.avg
+    average_examples_per_sec = params.total_batch_size / batch_time.avg
+    cur_examples_per_sec_without_data = params.total_batch_size / train_time.val
+    cur_examples_per_sec = params.total_batch_size / batch_time.val
+    print("average_examples_per_sec_without_data: {}".format(
+		average_examples_per_sec_without_data))
+    print("average_examples_per_sec: {}".format(average_examples_per_sec))
+    print("cur_examples_per_sec_without_data: {}".format(
+		cur_examples_per_sec_without_data))
+    print("cur_examples_per_sec: {}".format(cur_examples_per_sec))
 
 def validate(val_loader, model, criterion, params):
   batch_time = AverageMeter()
@@ -312,11 +308,12 @@ def validate(val_loader, model, criterion, params):
 
       batch_time.update(time.time() - end)
       end = time.end()
-      print("Test: [{0}/{1}]\t"
-            "Time: {batch_time.val: .3f} ({batch_time.avg: .3f})\t"
-            "Loss: {loss.val: .4f} ({loss.avg: .4f})\t"
-            "Acc@1: {top1.val: .3f} ({top1.avg: .3f})\t"
-            "Acc@5: {top5.val: .3f} ({top5.avg: .3f})\t".format(
+      print("*" * 30)
+      print("Test: [{0}/{1}]\n"
+            "Time: {batch_time.val: .3f} ({batch_time.avg: .3f})\n"
+            "Loss: {loss.val: .4f} ({loss.avg: .4f})\n"
+            "Acc@1: {top1.val: .3f} ({top1.avg: .3f})\n"
+            "Acc@5: {top5.val: .3f} ({top5.avg: .3f})\n".format(
                 i, len(train_loader), batch_time=batch_time,
                 loss=losses, top1=top1, top5=top5))
 
